64e017675f49c19f042699b8213019e87b99937b
maneekbaasha <m.irphan@hotmail.fr>
2026-09-11 02:05:01 +0200

    design: load Apple-inspired helpdesk card

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import "./motion-system.css";
 import "./apple-viture-refinement.css";
 import "./projects-product-v2.css";
 import "./projects-utility-v1.css";
+import "./project-card-apple-v1.css";
 import "./sections-editorial-v2.css";
 import "./case-studies-cinematic.css";
 
